947313c5acdacc8c01539479a26718a721c222ef
Joël Maïzi <joel.maizi@collorg.org>
2015-10-18 19:31:52 +0200

    [WIP] Add config file in /etc/torm/{dbname}. Load fields.

--- torm/table.py
+++ torm/table.py
@@ -4,6 +4,7 @@
 import re
 import sys
 import psycopg2, psycopg2.extras
+from configparser import ConfigParser
 
 def init_table(self, **kwargs):
     self.bla = 'blabla'
@@ -14,14 +15,25 @@ def repr_table(self):
 
 class TableFactory(type):
     __deja_vu = {}
-    re_split_fqtn = re.compile('\"\.\"|\"\.|\.\"|^\"|\"$')
+    re_split_fqtn = re.compile(r'\"\.\"|\"\.|\.\"|^\"|\"$')
     def __new__(cls, clsname, bases, dct):
         TF = TableFactory
         tbl_attr = {}
         TF.__split_fqtn(dct['fqtn'], tbl_attr)
+        tbl_attr['conn'] = TF.__connect(tbl_attr['dbname'])
+        TF.__set_fields(tbl_attr)
         tbl_attr['__init__'] = init_table
         tbl_attr['__repr__'] = repr_table
         return super(TF, cls).__new__(cls, clsname, bases, tbl_attr)
+
+    @staticmethod
+    def __connect(dbname):
+        config = ConfigParser()
+        config.read('/etc/torm/{}'.format(dbname))
+        params = dict(config['database'].items())
+        params['dbname'] = dbname
+        return psycopg2.connect('dbname={dbname} host={host} user={user} '
+                                'password={password} port={port}'.format(**params))
 
     @staticmethod
     def __split_fqtn(fqtn, tbl_attr):
@@ -43,16 +55,17 @@ class TableFactory(type):
         tbl_attr['fqtn'] = '.'.join(['"{}"'.format(elt) for elt in sfqtn])
 
     @staticmethod
-    def todo():
-        con = psycopg2.connect('dbname={} host=localhost'.format(dbname))
-        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("select column_name from information_schema.columns "
+    def __set_fields(tbl_attr):
+        cur = tbl_attr['conn'].cursor(cursor_factory=psycopg2.extras.DictCursor)
+        ta = tbl_attr
+        cur.execute("select column_name from information_schema.columns"
                     " where "
-                    "table_catalog=%s and "
-                    "table_schema=%s and "
-                    "table_name=%s",
-                    (dbname, schemaname, tablename))
-        return cls
+                    " table_catalog=%s and"
+                    " table_schema=%s and"
+                    " table_name=%s",
+                    (ta['dbname'], ta['schemaname'], ta['tablename']))
+        for elt in cur.fetchall():
+            tbl_attr['{}_'.format(elt[0])] = None
 
 class OidTable(metaclass=TableFactory):
     fqtn = 'dpt_info."collorg.core".oid_table'
@@ -60,6 +73,7 @@ class OidTable(metaclass=TableFactory):
 if __name__ == '__main__':
     oidt = OidTable()
     print(type(oidt))
+    print(vars(oidt.__class__))
     print(oidt)
     sys.exit()
     print(table('a."b"."c"'))
