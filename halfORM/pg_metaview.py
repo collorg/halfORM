8369d99c598f69a8127cfa2ceff25c074f96d057
Joël Maïzi <joel.maizi@collorg.org>
2015-10-24 07:40:40 +0200

    Move PostgreSQL metadata request from model to pg_metaview module.

---
+++ halfORM/pg_metaview.py
@@ -0,0 +1,78 @@
+request = """
+SELECT
+    a.attrelid AS tableid,
+    array_agg( i.inhseqno::TEXT || ':' || i.inhparent::TEXT ) AS inherits,
+    c.relkind AS tablekind,
+    n.nspname AS schemaname,
+    c.relname AS tablename,
+    tdesc.description AS tabledescription,
+    a.attname AS fieldname,
+    adesc.description AS fielddescription,
+    a.attndims AS fielddim,
+    pt.typname AS fieldtype,
+    a.attnum AS fieldnum,
+    NOT( a.attislocal ) AS inherited,
+    cn_uniq.contype AS uniq,
+    a.attnotnull OR NULL AS notnull,
+    cn_pk.contype AS pkey,
+    cn_fk.contype AS fkey,
+    cn_fk.conname AS fkeyname,
+    cn_fk.conkey AS keynum,
+    cn_fk.confrelid AS fkeytableid,
+    cn_fk.confkey AS fkeynum
+FROM
+    pg_class c -- table
+    LEFT JOIN pg_description tdesc ON
+    tdesc.objoid = c.oid AND
+    tdesc.objsubid = 0
+    LEFT JOIN pg_namespace n ON
+    n.oid = c.relnamespace
+    LEFT JOIN pg_inherits i ON
+    i.inhrelid = c.oid
+    LEFT JOIN pg_attribute a ON
+    a.attrelid = c.oid
+    LEFT JOIN pg_description adesc ON
+    adesc.objoid = c.oid AND
+    adesc.objsubid = a.attnum
+    JOIN pg_type pt ON
+    a.atttypid = pt.oid
+    LEFT JOIN pg_constraint cn_uniq ON
+    cn_uniq.contype = 'u' AND
+    cn_uniq.conrelid = a.attrelid AND
+    a.attnum = ANY( cn_uniq.conkey )
+    LEFT JOIN pg_constraint cn_pk ON
+    cn_pk.contype = 'p' AND
+    cn_pk.conrelid = a.attrelid AND
+    a.attnum = ANY( cn_pk.conkey )
+    LEFT JOIN pg_constraint cn_fk ON
+    cn_fk.contype = 'f' AND
+    cn_fk.conrelid = a.attrelid AND
+    a.attnum = ANY( cn_fk.conkey )
+WHERE
+    n.nspname <> 'pg_catalog'::name AND
+    n.nspname <> 'information_schema'::name AND
+    ( c.relkind = 'r'::"char" OR c.relkind = 'v'::"char" ) AND
+    a.attnum > 0 -- AND
+GROUP BY
+    a.attrelid,
+    n.nspname,
+    c.relname,
+    tdesc.description,
+    c.relkind,
+    a.attnum,
+    a.attname,
+    adesc.description,
+    a.attndims,
+    a.attislocal,
+    pt.typname,
+    cn_uniq.contype,
+    a.attnotnull,
+    cn_pk.contype,
+    cn_fk.contype,
+    cn_fk.conname,
+    cn_fk.conkey,
+    cn_fk.confrelid,
+    cn_fk.confkey
+ORDER BY
+    n.nspname, c.relname, a.attnum
+"""
