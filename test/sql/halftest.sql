5bf6abd0695ebf8ff7c132f42462b8026fe913cd
Joël Maïzi <joel.maizi@collorg.org>
2015-10-26 18:27:42 +0100

    Replace \c halftest halftest and use grant to halftest instead.

--- test/sql/halftest.sql
+++ test/sql/halftest.sql
@@ -1,17 +1,20 @@
 create user halftest password 'halftest';
 create database halftest owner halftest;
 
-\c halftest halftest
+\c halftest
 
 create schema actor;
+grant all on schema actor to halftest;
 create table actor.person(
     first_name text,
     last_name text,
     birth_date date,
     primary key(first_name, last_name, birth_date)
 );
+grant all on table actor.person to halftest;
 
 create schema blog;
+grant all on schema blog to halftest;
 create sequence blog.id_post;
 create table blog.post(
     id int default nextval('blog.id_post') unique not null,
@@ -26,6 +29,8 @@ alter table blog.post add constraint "author"
     foreign key(a_first_name, a_last_name, a_birth_date)
     references actor.person(first_name, last_name, birth_date)
 	on update cascade on delete cascade;
+grant all on table blog.post to halftest;
+grant all on table blog.id_post to halftest;
 
 create sequence blog.id_comment;
 create table blog.comment(
@@ -45,3 +50,5 @@ alter table blog.comment add constraint "post"
     foreign key(id_post)
     references blog.post
 	on update cascade on delete cascade;
+grant all on table blog.comment to halftest;
+grant all on table blog.id_comment to halftest;
