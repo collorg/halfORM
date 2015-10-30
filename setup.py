7a1d6daccb39b4e5f4b3df064daa203f12e93bfb
Joël Maïzi <joel.maizi@collorg.org>
2015-10-30 18:55:36 +0100

    Restrict the installation with Python3.

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #-*- coding: utf-8 -*-
 
+import sys
 import os
 from setuptools import setup
+
+try:
+    assert sys.version_info.major >= 3
+except:
+    sys.stderr.write("ERROR! halfORM requires Python3.\n")
+    exit(1)
 
 def read(name):
     return open(os.path.join(os.path.dirname(__file__), name)).read()
@@ -9,7 +16,7 @@ def read(name):
 setup(
     name='halfORM',
     version='0.0.1',
-    description=" A simple ORM in Python only dealing with DML part of SQL.",
+    description="A simple ORM in Python only dealing with the DML part of SQL.",
     long_description=read('README.md'),
     keywords='',
     author='Joël Maïzi',
