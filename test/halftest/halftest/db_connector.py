f2d91af4624f366062e97e3e291792d832797bfa
Joël Maïzi <joel.maizi@collorg.org>
2016-11-29 11:52:50 +0100

    (test) Add halftest package.

---
+++ test/halftest/halftest/db_connector.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+
+"""This module exports the fonction base_relation_class which is
+imported by all modules in the package halftest.
+"""
+
+from half_orm.model import Model
+
+__all__ = ['base_relation_class']
+
+MODEL = Model('halftest', scope=__name__)
+
+def base_relation_class(qrn):
+    """Returns the class corresponding to the QRN (qualified relation name).
+    """
+    cls = MODEL._import_class(qrn, scope=scope)
+    return cls
