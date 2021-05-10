aaa86ddeb311b450f4a50049c3c20db113874be4
Joël Maïzi <joel.maizi@lirmm.fr>
2021-05-10 11:20:23 +0200

    Update license/setup.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author='Joël Maïzi',
     author_email='joel.maizi@collorg.org',
     url='https://github.com/collorg/half_orm',
-    license='GPL',
+    license='GPLv3',
     packages=['half_orm', 'half_orm/hop'],
     package_data={'half_orm': [
         'hop/templates/*',
@@ -32,4 +32,25 @@ setup(
             'hop=half_orm.hop:main',
         ],
     },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GPLv3 License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
 )
