69a403b921f39cfb8336b67466b18231dc31e80d
Joël Maïzi <joel.maizi@lirmm.fr>
2022-05-04 16:46:46 +0200

    [model][halftest] Add uniq constraint to last_name for actor.person.

--- test/sql/halftest.sql
+++ test/sql/halftest.sql
@@ -90,7 +90,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE actor.person (
     id integer DEFAULT nextval('actor.id_person'::regclass) NOT NULL,
-    first_name text NOT NULL,
+    first_name text UNIQUE NOT NULL,
     last_name text NOT NULL,
     birth_date date NOT NULL
 );
