a7f9f9e87b1dc5e1c72f818940ca16c02e78a960
Joël Maizi <joel.maizi@lirmm.fr>
2024-04-13 08:46:32 +0200

    WIP

--- half_orm/hotest.py
+++ half_orm/hotest.py
@@ -2,71 +2,70 @@
 
 import unittest
 from typing import List
-from half_orm.relation import Relation
 
 class HoTestCase(unittest.TestCase):
-    def hotAssertIsPkey(self, relation: Relation, field_names: List[str]):
+    def hotAssertIsPkey(self, Relation: "Relation class", field_names: List[str]):
         "it shoud be the primary key"
-        pkey = set(relation()._ho_pkey.keys())
+        pkey = set(Relation()._ho_pkey.keys())
         if set(field_names) != pkey:
             raise self.fail(f'PKey failure: {set(field_names)} != {pkey}')
 
-    def hotAssertIsUnique(self, relation: Relation, fields_names: List[str]):
-        "Checks if a list of fields is unique for the relation."
-        if (set(fields_names) == set(relation._ho_pkey.keys())):
+    def hotAssertIsUnique(self, Relation: "Relation class", fields_names: List[str]):
+        "Checks if a list of fields is unique for the Relation."
+        if (set(fields_names) == set(Relation._ho_pkey.keys())):
             return # OK it's the primary key
         for field_name in fields_names:
-            field = relation()._ho_fields[field_name]
+            field = Relation()._ho_fields[field_name]
             if not field._Field__metadata['uniq']:
                 raise self.fail(f"'{fields_names}' is not unique.")
 
-    def hotAssertIsNotNull(self, relation: Relation, field_name: str):
-        if not relation()._ho_fields[field_name].is_not_null():
+    def hotAssertIsNotNull(self, Relation: "Relation class", field_name: str):
+        if not Relation()._ho_fields[field_name].is_not_null():
             raise self.fail(f"'{field_name}' is not 'not null'.")
         
-    def hotAssertReferences(self, relation: Relation, fk_name: str, f_relation: Relation):
-        referenced = relation()._ho_fkeys[fk_name]()
-        if not referenced._qrn == f_relation._qrn:
-            raise self.fail(f"{relation.__class__.__name__}()._ho_fkeys['{fk_name}']() does not reference {f_relation.__name__}")
+    def hotAssertReferences(self, Relation: "Relation class", fk_name: str, FRelation: "Relation class"):
+        referenced = Relation()._ho_fkeys[fk_name]()
+        if referenced._qrn != FRelation._qrn:
+            raise self.fail(f"{Relation.__class__.__name__}()._ho_fkeys['{fk_name}']() does not reference {FRelation.__name__}")
 
-    def hotAssertAliasReferences(self, relation: Relation, alias: str, f_relation: Relation):
-        referenced = eval(f"relation().{alias}")
-        if not referenced()._qrn == f_relation._qrn:
-            raise self.fail(f"{relation.__class__.__name__}.{alias}() does not reference {f_relation.__name__}")
+    def hotAssertAliasReferences(self, Relation: "Relation class", alias: str, FRelation: "Relation class"):
+        referenced = eval(f"Relation().{alias}")
+        if referenced()._qrn != FRelation._qrn:
+            raise self.fail(f"{Relation.__class__.__name__}.{alias}() does not reference {FRelation.__name__}")
 
-    def __check_update_action(self, relation: Relation, fk_name: str, update_type: str):
-        return self.assertEqual(relation()._ho_fkeys[fk_name].confupdtype, update_type)
+    def __check_update_action(self, Relation: "Relation class", fk_name: str, update_type: str):
+        return self.assertEqual(Relation()._ho_fkeys[fk_name].confupdtype, update_type)
 
-    def __check_delete_action(self, relation: Relation, fk_name: str, delete_type: str):
-        return self.assertEqual(relation()._ho_fkeys[fk_name].confdeltype, delete_type)
+    def __check_delete_action(self, Relation: "Relation class", fk_name: str, delete_type: str):
+        return self.assertEqual(Relation()._ho_fkeys[fk_name].confdeltype, delete_type)
 
-    def hotAssertOnUpdateNoAction(self, relation: Relation, fk_name: str):
-        return self.__check_update_action(relation, fk_name, 'a')
+    def hotAssertOnUpdateNoAction(self, Relation: "Relation class", fk_name: str):
+        return self.__check_update_action(Relation, fk_name, 'a')
 
-    def hotAssertOnUpdateRestict(self, relation: Relation, fk_name: str):
-        return self.__check_update_action(relation, fk_name, 'r')
+    def hotAssertOnUpdateRestict(self, Relation: "Relation class", fk_name: str):
+        return self.__check_update_action(Relation, fk_name, 'r')
 
-    def hotAssertOnUpdateCascade(self, relation: Relation, fk_name: str):
-        return self.__check_update_action(relation, fk_name, 'c')
+    def hotAssertOnUpdateCascade(self, Relation: "Relation class", fk_name: str):
+        return self.__check_update_action(Relation, fk_name, 'c')
 
-    def hotAssertOnUpdateSetNull(self, relation: Relation, fk_name: str):
-        return self.__check_update_action(relation, fk_name, 'n')
+    def hotAssertOnUpdateSetNull(self, Relation: "Relation class", fk_name: str):
+        return self.__check_update_action(Relation, fk_name, 'n')
 
-    def hotAssertOnUpdateSetDefault(self, relation: Relation, fk_name: str):
-        return self.__check_update_action(relation, fk_name, 'd')
+    def hotAssertOnUpdateSetDefault(self, Relation: "Relation class", fk_name: str):
+        return self.__check_update_action(Relation, fk_name, 'd')
 
-    def hotAssertOnDeleteNoAction(self, relation: Relation, fk_name: str):
-        return self.__check_delete_action(relation, fk_name, 'a')
+    def hotAssertOnDeleteNoAction(self, Relation: "Relation class", fk_name: str):
+        return self.__check_delete_action(Relation, fk_name, 'a')
 
-    def hotAssertOnDeleteRestict(self, relation: Relation, fk_name: str):
-        return self.__check_delete_action(relation, fk_name, 'r')
+    def hotAssertOnDeleteRestict(self, Relation: "Relation class", fk_name: str):
+        return self.__check_delete_action(Relation, fk_name, 'r')
 
-    def hotAssertOnDeleteCascade(self, relation: Relation, fk_name: str):
-        return self.__check_delete_action(relation, fk_name, 'c')
+    def hotAssertOnDeleteCascade(self, Relation: "Relation class", fk_name: str):
+        return self.__check_delete_action(Relation, fk_name, 'c')
 
-    def hotAssertOnDeleteSetNull(self, relation: Relation, fk_name: str):
-        return self.__check_delete_action(relation, fk_name, 'n')
+    def hotAssertOnDeleteSetNull(self, Relation: "Relation class", fk_name: str):
+        return self.__check_delete_action(Relation, fk_name, 'n')
 
-    def hotAssertOnDeleteSetDefault(self, relation: Relation, fk_name: str):
-        return self.__check_delete_action(relation, fk_name, 'd')
+    def hotAssertOnDeleteSetDefault(self, Relation: "Relation class", fk_name: str):
+        return self.__check_delete_action(Relation, fk_name, 'd')
 
