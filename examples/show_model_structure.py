a4bf44b1fe7254787b449e65c3bfb2b3ece3000c
Joël Maizi <joel.maizi@lirmm.fr>
2024-08-26 09:25:48 +0200

    Remove examples directory.

--- examples/show_model_structure.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-from half_orm.model import Model
-
-db = Model('halftest')
-db.desc()
--- examples/show_relation_structure.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-from half_orm.model import Model
-
-model = Model('halftest')
-Person = model.get_relation_class('actor.person')
-print(Person())
